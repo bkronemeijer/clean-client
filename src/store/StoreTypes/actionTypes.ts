a9e21201787d2f42ae0632ae4c0dbe7c81b158b6
Betty <betty.k1995@gmail.com>
2020-07-22 15:42:43 +0200

    added model and action type files

--- src/store/StoreTypes/actionTypes.ts
+++ src/store/StoreTypes/actionTypes.ts
@@ -1,1 +1,14 @@
-export const defaultType = "toBeFilledIn"
+import {
+  User,
+  Household,
+  Task
+} from '../../Types/model'
+
+export type StoreState = {
+  users: User,
+  households: Household,
+  tasks: Task,
+  // appState: Appstate
+}
+
+export type GetState = () => StoreState

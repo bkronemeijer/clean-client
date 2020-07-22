d8ad65b8a76b6e29689dcc8b46da493f0864a33a
Betty <betty.k1995@gmail.com>
2020-07-22 16:01:21 +0200

    added reducer, action and thunk that fetch household with its corresponding users

--- src/pages/HouseholdPage/index.tsx
+++ src/pages/HouseholdPage/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchHouseholdWithUsers } from '../../store/household/actions'
+import { useDispatch } from 'react-redux'
 
 export default function HouseholdPage() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchHouseholdWithUsers(1))
+
+  }, [dispatch])
 
   return (
     <div>
--- src/store/household/actions.ts
+++ src/store/household/actions.ts
@@ -3,6 +3,11 @@ import axios from 'axios'
 import { Dispatch } from 'redux'
 import { GetState } from '../StoreTypes/actionTypes'
 
+export const householdWithUsersFetched = (payload: any) => ({
+  type: "HOUSEHOLD_USERS_FETCHED",
+  payload
+})
+
 export function fetchHouseholdWithUsers (householdId: number) {
   return async function thunk (dispatch: Dispatch, getState: GetState) {
     const fetchUrl = `${apiUrl}/household`
@@ -10,5 +15,10 @@ export function fetchHouseholdWithUsers (householdId: number) {
     const response = await axios.post(fetchUrl, {
       id: householdId
     })
+    // auth headers moeten nog worden toegevoegd
+
+    dispatch(householdWithUsersFetched(response.data))
+
+    console.log("hoi dit is een response", response)
   }
 }

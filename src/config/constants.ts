e3acb75d068cec63838752d29bf038b76a85e71d
Betty <betty.k1995@gmail.com>
2020-08-03 10:59:35 +0200

    change to force deploy

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000"
+export const apiUrl = process.env.API_URL || "https://dustly-backend.herokuapp.com"
 
 export const DEFAULT_MESSAGE_TIMEOUT = 2500
